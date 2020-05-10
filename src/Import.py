d78f2cf6e54dfb9a5a975e53ec53fd064128e3f7
johannesvollmer <johannes.boenki@t-online.de>
2020-05-10 18:09:59 +0200

    plot quantiles by threshold

---
+++ src/Import.py
@@ -0,0 +1,91 @@
+# convert the raw captured data to a simplified format, merging all experiments and demographic data into a single file
+
+
+from lib.throughput.Throughput import Throughput
+import json
+import os
+from os import path
+
+dataDir = os.getcwd() + f"\\data"
+
+class Phrase: 
+    def __init__(self, events, phrase):
+        startEvent = events.pop(0)
+        ownEvents = [ startEvent ]
+        transcribed = ""
+        deletions = 0
+        typedText = None
+        suggestion = None
+
+        while True:
+            event = events.pop(0)
+            ownEvents.append(event)
+
+            if "text" in event:
+                if len(event["text"]) < len(transcribed): 
+                    deletions += 1
+
+                typedText = event["text"]
+                transcribed = typedText
+
+            if "suggestion" in event:
+                suggestion = event["suggestion"]
+                transcribed = suggestion
+
+            if "next" in event or "suggestion" in event:
+                break
+
+        endEvent = ownEvents[-1]
+        target = phrase["target"]
+        durationMillis = endEvent["time"] - startEvent["time"]
+        throughput = Throughput(target = target, transcribed = transcribed, time = durationMillis)
+
+        self.target = target
+        self.durationMillis = durationMillis
+        self.transcribed = transcribed
+        self.deletions = deletions
+        self.typedText = typedText
+        self.selectedSuggestion = suggestion
+
+        self.suggestions = phrase["suggestions"]
+        self.threshold = phrase["threshold"]
+        self.duration = durationMillis * 0.001
+        self.keyPresses = len(ownEvents) - 2
+        self.charsThreshold = len(target) * (1 - phrase["threshold"])
+        self.throughput = throughput.throughput
+        self.charsPerSecond = throughput.cps
+        self.wordsPerMinute = 12 * throughput.cps
+        
+        self.plausible = len(transcribed) > 0
+
+
+class Experiment: 
+    def __init__(self, events, participant):
+        firstEvent = events.pop(0)
+        phrases = firstEvent["phrases"]
+        blocks = []
+
+        for _ in range(4):
+            _next_button = events.pop(0)
+            block = []
+            
+            for _ in range(6):
+                phrase = Phrase(events, phrases.pop(0))
+                if phrase.plausible: block.append(phrase)
+
+            blocks.append(block)
+        
+        _warmup = blocks.pop(0)
+        self.participant = participant
+        self.blocks = blocks
+
+
+def load():
+    participants = json.load(open(dataDir + "\\demographic.json"))
+    experiments = []
+
+    for index in range(len(participants)):
+        events = json.load(open(dataDir + f"\\experiment\\{index}.json"))
+        experiments.append(Experiment(events, participants.pop(0)))
+
+    return experiments
--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,41 @@
-from Import import extract
+from Import import load
+import numpy as num
+from functools import reduce
+import matplotlib.pyplot as plot
+
+print("loading...")
+experiments = load()
+print("finished.\n")
+
+print(f"participants: {len(experiments)}")
+
+females = list(filter(lambda experiment: experiment.participant["gender"] == "female", experiments))
+print(f"females: {len(females)} ({100 * len(females) / len(experiments)}%)")
+
+left = list(filter(lambda experiment: experiment.participant["hand"] == "left", experiments))
+print(f"left handed: {len(left)} ({100 * len(left) / len(experiments)}%)")
+
+medianAge = num.median(num.array(list(map(lambda exp: exp.participant["age"], experiments))))
+print(f"median age: {medianAge}")
+
+byThreshold = [ [], [], [], [], [], [] ]
+for experiment in experiments:
+    for block in experiment.blocks:
+        for phrase in block:
+            # if phrase.target in phrase.suggestions: # filter out phrases where the suggestions did not contain the target phrase
+                byThreshold[int((phrase.threshold + 0.001) * 5)].append(phrase)
+            
+throughputByThreshold = [[phrase.throughput for phrase in phrases] for phrases in byThreshold]
+
+
+thresholds = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+x = thresholds[::-1]
+
+quantiles = [[num.quantile(threshold, quantile) for threshold in throughputByThreshold] for quantile in [0, 0.25, 0.5, 0.75, 1.0]]
+
+figure, axes = plot.subplots()
+axes.plot(x, quantiles[2], '-')
+axes.fill_between(x, quantiles[0], quantiles[4], alpha=0.2)
+axes.fill_between(x, quantiles[1], quantiles[3], alpha=0.2)
+
+plot.show()
